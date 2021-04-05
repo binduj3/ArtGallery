e23fb09482e65d0f3622b2a691bd90fd1f0481a7
binduj3@gmail.com <binduj3@gmail.com>
2021-04-05 15:56:43 -0600

    encode uri for mongo

--- api/db/db.js
+++ api/db/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 
 const connectDB = async () => {
-  const conn = await mongoose.connect(process.env.MONGO_URI, {
+  const conn = await mongoose.connect(`${encodeURI(process.env.MONGO_URI)}`, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     useCreateIndex: true,
