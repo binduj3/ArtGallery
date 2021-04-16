3bdc4d2880b0e3024900a89cb60c1c7f98e587a5
binduj3@gmail.com <binduj3@gmail.com>
2021-04-15 21:04:27 -0600

    resize the carousel

--- front-client/src/components/pages/Gallery.js
+++ front-client/src/components/pages/Gallery.js
@@ -6,74 +6,77 @@ const Gallery = (props) => {
 
   return (
     <div className='container'>
-      <div>
-        <div className='my-5'>
-          <h1> {year} Gallery </h1>
-        </div>
-        <div
-          id='carouselExampleCaptions'
-          className='carousel slide'
-          data-ride='carousel'
-        >
-          <ol className='carousel-indicators'>
-            {props.location.state.document &&
-              props.location.state.document.length > 0 &&
-              props.location.state.document.map((doc, index) => {
-                return (
-                  <li
-                    data-target='#carouselExampleCaptions'
-                    data-slide-to={index}
-                    className={`${index === 0 && "active"}`}
-                  ></li>
-                );
-              })}
-          </ol>
-
-          <div className='carousel-inner'>
-            {props.location.state.document &&
-              props.location.state.document.length > 0 &&
-              props.location.state.document.map((doc, index) => {
-                return (
-                  <div
-                    className={`carousel-item  ${index === 0 && "active"}`}
-                    key={index}
-                  >
-                    <img
-                      src={doc.storageUrl}
-                      className='d-block w-100'
-                      alt='Pictures'
-                    />
-                    <div className='carousel-caption d-none d-md-block'>
-                      <p>{doc.description}</p>
-                    </div>
-                  </div>
-                );
-              })}
+      <div className='row'>
+        <div className='col-md-6 offset-md-3 col-12 '>
+          <div className='my-5'>
+            <h1> {year} Gallery </h1>
           </div>
-          <a
-            className='carousel-control-prev'
-            href='#carouselExampleCaptions'
-            role='button'
-            data-slide='prev'
+          <div
+            id='carouselExampleCaptions'
+            className='carousel slide'
+            data-ride='carousel'
           >
-            <span
-              className='carousel-control-prev-icon'
-              aria-hidden='true'
-            ></span>
-            <span className='sr-only'>Previous</span>
-          </a>
-          <a
-            className='carousel-control-next'
-            href='#carouselExampleCaptions'
-            role='button'
-            data-slide='next'
-          >
-            <span
-              className='carousel-control-next-icon'
-              aria-hidden='true'
-            ></span>
-            <span className='sr-only'>Next</span>
-          </a>
+            <ol className='carousel-indicators'>
+              {props.location.state.document &&
+                props.location.state.document.length > 0 &&
+                props.location.state.document.map((doc, index) => {
+                  return (
+                    <li
+                      key={index}
+                      data-target='#carouselExampleCaptions'
+                      data-slide-to={index}
+                      className={`${index === 0 && "active"}`}
+                    ></li>
+                  );
+                })}
+            </ol>
+
+            <div className='carousel-inner'>
+              {props.location.state.document &&
+                props.location.state.document.length > 0 &&
+                props.location.state.document.map((doc, index) => {
+                  return (
+                    <div
+                      className={`carousel-item  ${index === 0 && "active"}`}
+                      key={index}
+                    >
+                      <img
+                        src={doc.storageUrl}
+                        className='d-block w-100'
+                        alt='Pictures'
+                      />
+                      <div className='carousel-caption d-none d-md-block'>
+                        <p>{doc.description}</p>
+                      </div>
+                    </div>
+                  );
+                })}
+            </div>
+            <a
+              className='carousel-control-prev'
+              href='#carouselExampleCaptions'
+              role='button'
+              data-slide='prev'
+            >
+              <span
+                className='carousel-control-prev-icon'
+                aria-hidden='true'
+              ></span>
+              <span className='sr-only'>Previous</span>
+            </a>
+            <a
+              className='carousel-control-next'
+              href='#carouselExampleCaptions'
+              role='button'
+              data-slide='next'
+            >
+              <span
+                className='carousel-control-next-icon'
+                aria-hidden='true'
+              ></span>
+              <span className='sr-only'>Next</span>
+            </a>
+          </div>
         </div>
       </div>
     </div>
